fbbee54d2940e15ddacdc1e079bef95d0ff09d14
Vasudev Kesharwani <kesharwanivasudev@gmail.com>
2022-05-31 01:32:15 +0530

    complete the inital code of searchNearby algo

---
+++ app.js
@@ -0,0 +1,70 @@
+
+const express = require("express");
+const axios = require('axios');
+const app = express();
+const bodyParser = require("body-parser");
+require('dotenv').config();
+const api_key = process.env.API_KEY ;
+const nearby_link = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+//sagar  my home  lat : 23.8479, lon : 78.6645
+//indoer vallabh nagar lat : 22.7284907 , lon : 75.8729906
+
+
+// Json Objects
+const alertData = {
+    device_id : "",
+     location : { 
+      lat : 22.7284907 
+      , lon : 75.8729906
+                  
+    },
+    svr_level : ""
+}
+
+
+
+//get method 
+app.get("/",(req,res)=>{
+    res.send(" server is  running " + res.statusCode );
+    NearbySearch(alertData.location,50000,"police");
+});
+
+
+//custom functions
+
+function NearbySearch(location,radius,type){
+    var config = {
+      method: 'get',
+      url:url(nearby_link,location,radius,api_key,type,"cruise"),
+      headers: { }
+    };
+    console.log(config.url);
+    axios(config)
+    .then(function (response) {
+     console.log("statusCode : " + response.statusCode );
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+// url generator 
+function url(link,location,radius,api_key,type,keyword){
+  const url = link + "location=" + location.lat +","+ location.lon + "&radius=" + radius +"&type=" + type + "&keyword="+ keyword + "&key=" + api_key ;
+  return url ;
+}
+
+
+
+
+
+
+
+
+app.listen(3000,()=>{
+    console.log("server is starting at port 3000");
+});
+
