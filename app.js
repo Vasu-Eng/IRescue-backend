8f3f7deae7897a1ca4814b0fe1b69e08d8417cd4
Vasudev Kesharwani <kesharwanivasudev@gmail.com>
2022-06-01 16:41:15 +0530

    successfully achieved the contact no of listed places, edited the work_status.txt

--- app.js
+++ app.js
@@ -6,22 +6,22 @@ const bodyParser = require("body-parser");
 require('dotenv').config();
 const api_key = process.env.API_KEY;
 const nearby_link = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?";
-
+const details_link = "https://maps.googleapis.com/maps/api/place/details/json?";
+var ResposeData; // places library response obeject 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
 
 //sagar-  (my home)        lat : 23.8479, lon : 78.6645
 //indore- vallabh nagar    lat : 22.7284907 , lon : 75.8729906
 
 
-// Json Objects  data 
+// Json Objects imported data 
 
 // send by hardware device 
 const alertData = {
   device_id: "",
   location: {
-    lat : 23.8479, lon : 78.6645
+    lat: 23.8479, lon: 78.6645
   },
   svr_level: ""
 }
@@ -34,13 +34,13 @@ const contactData = {
     email_id: "",
     address: "",
     relation_: ""
-  } ,
-person_2: {
+  },
+  person_2: {
     contact_no: [],
     email_id: "",
     address: "",
     relation_: ""
-  } ,
+  },
   person_3: {
     contact_no: [],
     email_id: "",
@@ -49,48 +49,68 @@ person_2: {
   }
 }
 
-
-
+//--------------------------------------------------- processing code ------------------------------------------------->
 
 //get method 
 app.get("/", (req, res) => {
   res.send(" server is  running " + res.statusCode);
-  NearbySearch(alertData.location, "hospital");
+  // nearbysearch algorithm
+  placesAPIResponseHandle("hospital");
 });
 
 
 //custom functions
-
-function NearbySearch(location,keyword) {
+function placesAPIResponseHandle(nearbysearch) {
   var config = {
     method: 'get',
-    url: url(nearby_link, location, api_key,keyword,"distance"),
+    url: url(nearby_link,api_key,parameters,nearbysearch,"null"),//hospital use last parameter for nearbysearch only
     headers: {}
   };
-  console.log(config.url);
   axios(config)
     .then(function (response) {
-      console.log("statusCode : " + response.statusCode);
-      console.log(response.data);
+      ResposeData = response.data ;
+      console.log("statusCode : " + ResposeData.status);
+       Place_id =  ResposeData.results[0].place_id
+      console.log("place_id :"+ Place_id);
+      console.log("Places API for nearby "+ nearbysearch + " : " + config.url );
+      place_details(ResposeData,3);
     })
     .catch(function (error) {
       console.log(error);
     });
+return 0;
 }
-// url generator 
-function url(link, location, api_key, keyword,rankby) {
-  const url = link + "location=" + location.lat + "," + location.lon  + "&keyword=" + keyword + "&key=" + api_key + "&rankby=" + rankby;
+function place_details(responseData,num) {
+  for(var i=0; i<num ;i++){
+  var Place_id = responseData.results[i].place_id;
+  console.log(url(details_link, api_key, parameters, "null", Place_id));
+  }
+}
+//url generator ------>
+var parameters = {
+  location: alertData.location,
+  rankby: "distance",
+  fields: "name,formatted_phone_number,rating"
+}
+function url(link, key, parameters, keyword, Place_id) {
+  var url = link + "key=" + key + "&location=" + parameters.location.lat + "," + parameters.location.lon;
+  if (link === nearby_link) {
+    url = url + "&keyword=" + keyword + "&rankby=" + parameters.rankby;
+  }
+  else if (link === details_link) {
+    url = url + "&fields=" + parameters.fields + "&place_id=" + Place_id;
+  }
   return url;
 }
-
 // In developing stage 
-function shortListPlaces(){ 
+function shortListPlaces() {
   // use the google response.data and sort the data according the function input parameter 
 }
+
 
 
 
 app.listen(3000, () => {
   console.log("server is starting at port 3000");
 });
-
+//------------------------------- END --------------------------
